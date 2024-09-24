085f709dc05ce48a5ccb3a16fadd42e7528fd8e2
UsuarioCardenas <diego.cardenas.g@tecsup.edu.pe>
2024-09-23 22:16:22 -0500

    First Commit

--- app/src/test/java/com/cardenas/diego/laboratoriocalificado01/Ejercicio01Kotlin.kt
+++ app/src/test/java/com/cardenas/diego/laboratoriocalificado01/Ejercicio01Kotlin.kt
@@ -7,7 +7,7 @@ class Ejercicio01Kotlin {
         val cadena2="silent" // declaramos la cadena de "silent"
         println("La palabra $cadena1 y $cadena2 son anagramas?: ${sonAnagramas(cadena1,cadena2)}") // imprimimos el resultado
         val cadena3="hello" // declaramos la cadena de "hello"
-        val cadena4="world" // declaramos la cadena de "worl"
+        val cadena4="world" // declaramos la cadena de "d"
         println("La palabra $cadena3 y $cadena4 son anagramas?: ${sonAnagramas(cadena3,cadena4)}") // imprimimos el resultado
     }
     private fun sonAnagramas(
@@ -15,10 +15,10 @@ class Ejercicio01Kotlin {
         cadena2:String, // pegunda cadena de texto
     ):Boolean{
         val cadena1LowerCase=cadena1.lowercase().toList().sorted() // convertimos la primera cadena a minúsculas,
-        // lo convertimos en una lista de caracteres y la ordenamos
+        // lo convertimos en una lista de caracteres y la ordenamos por orden alfabetico para poder tener las letras y su cantidad
         val cadena2LowerCase=cadena2.lowercase().toList().sorted() // convertimos la primera cadena a minúsculas,
-        // lo convertimos en una lista de caracteres y la ordenamos
-        // retornamos true si ambas listas de caracteres ordenadas son iguales
+        // lo convertimos en una lista de caracteres y la ordenamos por orden alfabetico para poder tener las letras y su cantidad
+        // retornamos true si las listas ordenadas son iguales
         return cadena1LowerCase==cadena2LowerCase
     }
 }
